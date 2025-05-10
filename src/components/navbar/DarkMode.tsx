e65e0470ca615580f53f650716c5f1fadf91bdf8
Felixdrawe <131857058+Felixdrawe@users.noreply.github.com>
2025-05-10 14:11:42 +0200

    added new components

---
+++ src/components/navbar/DarkMode.tsx
@@ -0,0 +1,3 @@
+export default function DarkMode() {
+  return <button>Toggle Dark Mode</button>;
+}
---
+++ src/components/navbar/NavSearch.tsx
@@ -0,0 +1,3 @@
+export default function NavSearch() {
+  return <input type="text" placeholder="Search..." />;
+}
